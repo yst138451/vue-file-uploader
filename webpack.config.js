0259237a8b477b581dfc2ea81a3113e961b4e5b7
yst <yomuro311@gmail.com>
2020-09-06 16:34:04 +0700

    Version bump, npmignore test files

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     index: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    filename: 'vue-file-uploader.min.js'
   },
   module: {
     rules: [
